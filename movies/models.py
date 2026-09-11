6438aaf5c1fbab17e4d267a46f624a74ecf8d157
Michael Tokuda <michael.tokuda17@gmail.com>
2026-09-11 18:43:32 -0400

    first version

--- movies/models.py
+++ movies/models.py
@@ -16,6 +16,7 @@ class Review(models.Model):
     id = models.AutoField(primary_key=True)
     comment = models.CharField(max_length=255)
     date = models.DateTimeField(auto_now_add=True)
+    reported = models.BooleanField(default=False)
     movie = models.ForeignKey(Movie,
         on_delete=models.CASCADE)
     user = models.ForeignKey(User,
